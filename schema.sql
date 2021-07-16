a17e3b07a75b3a1f9926dd2e49fe4e669573af67
Jason Bailey <jmbailey816@gmail.com>
2021-07-16 11:14:02 -0400

    DB and employee table created with seed data

---
+++ schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE DATABASE employeetracker_db;
+USE employeetracker_db;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    department VARCHAR(100) NOT NULL,
+    salary INT(10) NOT NULL,
+    manager VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO employee (first_name, last_name, title, department, salary, manager)
+VALUES ("Abby", "Allred", "Full Stack Developer", "Engineering", 100000, "Mary Mulligan"), ("Bob", "Benson", "Lawyer", "Legal", 125000, "Neal Notts"), ("Chris", "Corningsworth", "Sales Rep", "Sales", 70000, "Owen Orville");
+
+SELECT * FROM employee;
