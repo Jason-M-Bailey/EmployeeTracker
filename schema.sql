9dc1b5e4bba374208621142ccf2122bd2f386f8d
Jason Bailey <jmbailey816@gmail.com>
2021-07-16 11:18:53 -0400

    Departments table setup

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS employeetracker_db;
 CREATE DATABASE employeetracker_db;
 USE employeetracker_db;
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
@@ -13,7 +13,16 @@ CREATE TABLE employee (
     PRIMARY KEY (id)
 );
 
-INSERT INTO employee (first_name, last_name, title, department, salary, manager)
+INSERT INTO employees (first_name, last_name, title, department, salary, manager)
 VALUES ("Abby", "Allred", "Full Stack Developer", "Engineering", 100000, "Mary Mulligan"), ("Bob", "Benson", "Lawyer", "Legal", 125000, "Neal Notts"), ("Chris", "Corningsworth", "Sales Rep", "Sales", 70000, "Owen Orville");
 
-SELECT * FROM employee;
+SELECT * FROM employees;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    department VARCHAR(100) NOT NULL,
+    manager VARCHAR(100) NOT NULL,
+    total_salary INT(10) NOT NULL,
+    PRIMARY KEY (id)
+)
+
